5eb418d71a756672be1d369a55cc8ac1b07ba435
Aleksandr <kornedj@gmail.com>
2022-02-26 20:33:16 +0300

    GIT, Первые шаги\.

---
+++ src/com/GeekBrains/HomeWork1.java
@@ -0,0 +1,10 @@
+package com.GeekBrains;
+
+public class HomeWork1 {
+    public static void main(String[] args) {
+        task1 ();
+    }
+    public static void task1() {
+        System.out.println("Hello, world!");
+    }
+}
